8ab07a2871d7f1da8466a177fb985ddb3a8191b6
levisre <levintaeyeon@live.com>
2017-03-07 09:54:13 +0700

    Add comments

--- make_keypair.rb
+++ make_keypair.rb
@@ -1,7 +1,10 @@
+# Create Keypair for use in RSA Encryption
 require 'openssl'
 
+# Key size is 4096 bit (RSA-4096)
 key_pair = OpenSSL::PKey::RSA.generate(4096)
 
 pub_key = key_pair.public_key
+# Export Private Key and Public Key to PEM format
 File.open("private_key", "w") { |f| f.write(key_pair.to_pem) }
 File.open("pubkey.pub", "w") { |f| f.write(pub_key.to_pem) }
